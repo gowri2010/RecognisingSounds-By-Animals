2492a766a79cfde3116d18e26c3520e566dff70b
gowri2010 <86656713+gowri2010@users.noreply.github.com>
2022-01-24 15:25:03 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function startClassification()
 {
     navigator.mediaDevices.getUserMedia({ audio: true});
-    classifier = ml5.soundClassifier('model.json', modelReady);
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/Nv4Ouidq2/model.json', modelReady);
 }
 
 function modelReady(){
